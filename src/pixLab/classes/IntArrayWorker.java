54602af9c65b12f83ec6b9efabb89116fa06bb50
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-12-18 13:42:19 -0700

    Added methods for largest and colTotal

--- src/pixLab/classes/IntArrayWorker.java
+++ src/pixLab/classes/IntArrayWorker.java
@@ -112,5 +112,23 @@ public class IntArrayWorker
 	  }
 	  return numberOfTimes;
   }
+  
+  public int getLargest() {
+	  int largestValue = 0;	
+	  for(int row = 0; row < matrix.length; row++) {
+		  for(int col = 0; col < matrix[0].length; col++) {
+			  if(matrix[row][col] > largestValue) largestValue = matrix[row][col];
+			}
+	  }
+	  return largestValue;
+  }
+  
+  public int getColTotal(int specificCol) {
+	  int total = 0;
+	  for(int row = 0; row < matrix.length; row++) {
+		  total += matrix[row][specificCol];
+	  }
+	  return total;
+  }
  
 }
